0801ecbea25705172797e50bc7b5b058dcf2947d
Michael Petö <michael@petoe.me>
2017-10-14 00:23:04 +0200

    Add spec for single, batch and block request

--- spec/mixpanel_spec.cr
+++ spec/mixpanel_spec.cr
@@ -1,9 +1,51 @@
 require "./spec_helper"
+require "secure_random"
 
 describe Mixpanel do
-  # TODO: Write tests
+  describe "Tracker" do
+    tracker = Mixpanel::Tracker.new "63777d70db1a6b84ac6936c4d07e9155"
+    spec_run = SecureRandom.uuid # Unique identifier for this run.
 
-  it "works" do
-    false.should eq(true)
+    it "single request" do
+      properties = Mixpanel::Event::Properties{
+        "CONTINUOUS_INTEGRATION" => ENV["CI"]? || false, 
+        "type" => "single",
+        "run" => spec_run,
+        "Random Number" => rand 10
+      }
+      tracker.track "spec", properties
+    end
+
+    it "batch request" do
+      events = Array(Mixpanel::Event).new(3) do |i|
+        Mixpanel::Event.new "spec", Mixpanel::Event::Properties{
+          "CONTINUOUS_INTEGRATION" => ENV["CI"]? || false, 
+          "type" => "batch",
+          "run" => spec_run,
+          "Random Number" => rand 10
+        }
+      end
+      tracker.track events
+    end
+
+    it "block request" do
+      tracker.track "spec" do |properties|
+         properties["CONTINUOUS_INTEGRATION"] = ENV["CI"]? || false
+         properties["type"] = "block"
+         properties["run"] = spec_run
+         properties["Random Number"] = rand 10
+      end
+    end
+
+    it "generate url" do
+      url = tracker.track_url "spec", Mixpanel::Event::Properties{
+        "CONTINUOUS_INTEGRATION" => ENV["CI"]? || false, 
+        "type" => "generate",
+        "run" => spec_run,
+        "Random Number" => rand 10
+      }, redirect: "https://petoe.me/"
+
+      "https://petoe.me/".should eq `curl -Ls -o /dev/null -w %{url_effective} #{url.to_s}`
+    end
   end
 end
